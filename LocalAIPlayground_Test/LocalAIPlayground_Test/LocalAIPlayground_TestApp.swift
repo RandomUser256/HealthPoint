d69816b74db1927a94d0c6de5c54d24e839edc91
Máximo Magallanes <maximomagallanes2006@gmail.com>
2026-04-12 21:24:01 -0700

    Chatbot Funcional
    Chatbot funcional con diferentes personalidades y una estructura general para que lea la base de datos futura

---
+++ LocalAIPlayground_Test/LocalAIPlayground_Test/LocalAIPlayground_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAIPlayground_TestApp.swift
+//  LocalAIPlayground_Test
+//
+//  Created by Máximo Magallanes Urtuzuástegui on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalAIPlayground_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LocalAIView()
+        }
+    }
+}
